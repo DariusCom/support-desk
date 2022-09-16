ad062c4cce12a35851218328e80480cd9ce9c6a2
DariusC <comanescudarius@gmail.com>
2022-09-16 17:10:31 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,15 +20,12 @@ app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 // Serve Frontend
 if (process.env.NODE_ENV === "production") {
-  // Set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
-  );
-} else {
   app.get("/", (req, res) => {
     res.json({ message: "Welcome to the Support Desk API" });
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "Set it to production, mate!" });
   });
 }
 
